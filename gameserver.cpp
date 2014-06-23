03b0c3d611af38649f302d5fe6ca1105e72c0cfe
Joao Cordeiro <jlcordeiro@gmail.com>
2014-06-24 00:15:17 +0100

    extracter room dispatch from header file

--- gameserver.cpp
+++ gameserver.cpp
@@ -3,5 +3,6 @@
 int main(int argc, const char *argv[])
 {
     GameRoom g;
+    g.dispatch();
     return 0;
 }
--- room.cpp
+++ room.cpp
@@ -85,3 +85,68 @@ const map<int, shared_ptr<User> >& Room::getUsers() const
 {
     return _users;
 }
+
+// ---
+
+void GameRoom::dispatch() {
+    setName("Room1");
+    setMaxUsers(4);
+    setVariable("var1", "3");
+
+    while (1) {
+        char buffer[BUF_SIZE] = "";
+
+        vector<string> new_messages;
+        int res = _socket.recv(new_messages);
+        //             if (res < 0) {
+        //                 return;
+        //             }
+
+        for (auto buffer : new_messages) {
+            std::string err;
+            cout << "<< " << buffer << endl;
+
+            auto json = Json::parse(buffer, err);
+            if (protocol::Join::validate_reply(buffer)) {
+                string user_name = json[protocol::Join::tag]["user"].string_value();
+                cout << user_name << " joined!" << endl;
+
+                shared_ptr<User> user = shared_ptr<User>(new User());
+                user->setName(user_name);
+                addUser(user);
+            }
+
+            if (protocol::Quit::validate_reply(buffer)) {
+                string user_name = json[protocol::Quit::tag]["user"].string_value();
+                cout << user_name << " has quit!" << endl;
+
+                auto user = getUserByName(user_name);
+                removeUser(user);
+            }
+
+            if (protocol::UVar::validate(buffer)) {
+                auto obj = json[protocol::UVar::tag];
+                string user_name = obj["user"].string_value();
+                string name = obj["name"].string_value();
+                string value = obj["value"].string_value();
+
+                auto user = getUserByName(user_name);
+                user->setVariable(name, value);
+            }
+        }
+
+        for (auto varp : getVariables()) {
+            cout << "~ " << varp.first << " => " << varp.second << endl;
+        }
+
+        for (auto u : _users) {
+            cout << "<<< [U] " << u.second->getName() << endl;
+            for (auto varp : u.second->getVariables()) {
+                cout << "      - " << varp.first << " => " << varp.second << endl;
+            }
+        }
+
+        cout << "@@ " << getUserCount() << endl;
+        sleep(1);
+    }
+}
