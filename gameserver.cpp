abd03b4a836d397187c2c7ea3fed10970935763f
joao <joao@gimo.co.uk>
2014-08-19 18:00:31 +0100

    added quit handler

--- gameserver.cpp
+++ gameserver.cpp
@@ -9,12 +9,21 @@ static void handleJoin(Entity& sender, const std::string& buffer)
     auto json = Json::parse(buffer, err);
     if (protocol::Join::validate_reply(buffer)) {
         string user_name = json[protocol::Join::tag]["user"].string_value();
-        cout << user_name << " joined!" << endl;
-
         auto comm = shared_ptr<Communication>(new NoCommunication());
         auto user = shared_ptr<Entity>(new Entity(comm, 1));
         user->setName(user_name);
         sender.addRelation(user);
+    }
+}
+
+static void handleQuit(Entity& sender, const std::string& buffer)
+{
+    string err;
+    auto json = Json::parse(buffer, err);
+    if (protocol::Quit::validate_reply(buffer)) {
+        string user_name = json[protocol::Quit::tag]["user"].string_value();
+        auto user = sender.getRelationByName(user_name);
+        sender.removeRelation(user);
     }
 }
 
@@ -33,6 +42,7 @@ int main(int argc, const char *argv[])
 
     g->acceptMessageVisitor(print_buffer);
     g->acceptMessageVisitor(handleJoin);
+    g->acceptMessageVisitor(handleQuit);
 
     g->setName(string(argv[1]));
     g->setMaxRelations(4);
--- room.cpp
+++ room.cpp
@@ -4,24 +4,8 @@
 
 // void GameRoom::dispatch(Entity e, const string& buffer) {
 //     std::string err;
-// 
 //     auto json = Json::parse(buffer, err);
-//     if (protocol::Join::validate_reply(buffer)) {
-//         string user_name = json[protocol::Join::tag]["user"].string_value();
-//         cout << user_name << " joined!" << endl;
 // 
-//         shared_ptr<Entity> user = shared_ptr<Entity>(new Entity(USER, 1));
-//         user->setName(user_name);
-//         e->addRelation(user);
-//     }
-// 
-//     if (protocol::Quit::validate_reply(buffer)) {
-//         string user_name = json[protocol::Quit::tag]["user"].string_value();
-//         cout << user_name << " has quit!" << endl;
-// 
-//         auto user = getRelationByName(user_name);
-//         e->removeRelation(user);
-//     }
 // 
 //     if (protocol::RVar::validate(buffer)) {
 //         e->handleVariable<Entity>(shared_from_this(), json[protocol::RVar::tag]);
